6395de6bc5402878b5e180b9e9e178a216b59250
Hank Shen <swh@admpub.com>
2023-06-17 17:00:31 +0800

    update

--- biz/nftables_interface_forward.go
+++ biz/nftables_interface_forward.go
@@ -86,30 +86,3 @@ func (nft *NFTables) forwardInterfaceRules(c *nftables.Conn) error {
 	c.AddRule(rule)
 	return nil
 }
-
-func (nft *NFTables) natInterfaceRules(c *nftables.Conn) error {
-	if len(nft.wanIface) == 0 || len(nft.wanIP) == 0 || nft.wanIP.IsUnspecified() {
-		return nil
-	}
-
-	// cmd: nft add rule ip nat postrouting meta oifname "eth0" \
-	// snat 192.168.0.1
-	// --
-	// oifname "eth0" snat to 192.168.15.11
-	exprs := make([]expr.Any, 0, 10)
-	exprs = append(exprs, utils.SetOIF(nft.wanIface)...)
-	exprs = append(exprs, utils.ExprImmediate(nft.wanIP))
-	switch nft.tNAT.Family {
-	case nftables.TableFamilyIPv4:
-		exprs = append(exprs, utils.ExprSNAT(1, 0))
-	case nftables.TableFamilyIPv6:
-		exprs = append(exprs, utils.ExprSNATv6(1, 0))
-	}
-	rule := &nftables.Rule{
-		Table: nft.tNAT,
-		Chain: nft.cPostrouting,
-		Exprs: exprs,
-	}
-	c.AddRule(rule)
-	return nil
-}
